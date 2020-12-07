7663a27f1bc13017278f86ef8e03685e10dd8b74
Lauren McCoy <lm_lutec@outlook.com>
2020-12-07 12:06:49 -0500

    added select dropdown

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { API } from 'aws-amplify';
 import { listTasks } from './graphql/queries';
 import { createTask as createTaskMutation, updateTask as updateTaskMutation, deleteTask as deleteTaskMutation } from './graphql/mutations';
 import React, { useState, useEffect } from 'react';
+import Select from "react-dropdown-select";
 
+
+// use bootstrap to style buttons
 const initialFormState = { name: '', category: '', description: '', due: '', complete: false }
 
 
@@ -12,6 +15,15 @@ function App() {
     const [tasks, setTasks] = useState([]);
     const [formData, setFormData] = useState(initialFormState);
     const [goalStatus, setGoalStatus] = useState(initialFormState.complete)
+    const options = [
+      { value: 'General', label: 'General' },
+      { value: 'Bill', label: 'Bill' },
+      { value: 'Work', label: 'Work' },
+      { value: 'School', label: 'School' },
+      { value: 'Workout', label: 'Workout' },
+      { value: 'Shopping/Need to buy', label: 'Shopping/Need to buy' },
+      { value: 'Appointment', label: 'Appointment' }
+    ]
 
     useEffect(() => {
         fetchTasks();
@@ -49,7 +61,7 @@ function App() {
         setTasks([ ...tasks, formData ]);
         setFormData(initialFormState);
       }
-    
+
       async function deleteTask({ id }) {
         const newTasksArray = tasks.filter(task => task.id !== id);
         setTasks(newTasksArray);
@@ -60,7 +72,8 @@ function App() {
       <header className="App-header" id="next_goal">
         <h1>Next Goal</h1>
                   <input onChange={e => setFormData({ ...formData, 'name': e.target.value})} placeholder="Goal name" value={formData.name}/><br/>
-                  <input onChange={e => setFormData({ ...formData, 'category': e.target.value})} placeholder="Category" value={formData.category}/><br/>
+                  <Select options={options} onChange={e => setFormData({ ...formData, 'category': e.traget.value})} value={formData.category} /><br/>
+                  {/* <input onChange={e => setFormData({ ...formData, 'category': e.target.value})} placeholder="Category" value={formData.category}/><br/> */}
                   <input onChange={e => setFormData({ ...formData, 'description': e.target.value})} placeholder="Goal description" value={formData.description}/><br/>
                   <input onChange={e => setFormData({ ...formData, 'due': e.target.value})} placeholder="Due" value={formData.due}/><br/>
                   <button onClick={createTask}>Create Goal</button>
@@ -70,6 +83,7 @@ function App() {
             tasks.map(task => (
               <div key={task.id || task.name} style={{display: task.complete === false ? 'block' : 'none'}} className="goallist">
                 <h2>{task.name}</h2>
+                <i>{task.category}</i>
                 <p>{task.description}</p>
                 <p style={{display: formData.due !== "" ? 'block': 'none'}}>
                   {task.due}
